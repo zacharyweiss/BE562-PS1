ea4d0f9fbba129b58a15421c04f386d378fc7078
Zachary Weiss <zachary.weiss.acct@gmail.com>
2019-09-12 15:39:19 -0400

    Initial upload

---
+++ ps1-dotplot.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+####
+# BE562: Problem Set 1 - string hashing/dotplots
+#
+# INSTRUCTIONS FOR USE:
+# call program as follows:
+#   ./ps1-dotplot.py <FASTA 1> <FASTA 2> <PLOTFILE>
+#   e.g. ./ps1-dotplot.py human-hoxa-region.fa mouse-hoxa-region.fa dotplot.jpg
+#
+# Make sure the ps1-dotplot.py is marked as executable:
+#   chmod +x ps1-dotplot.py
+# or in windows with:
+#   python ps1-dotplot.py human-hoxa-region.fa mouse-hoxa-region.fa dotplot.jpg
+# once you have put python in your path
+#
+# matplotlib
+# matplotlib is used to generate plots for this problem. It is a widely used
+# python package. Installation instructions can be found at
+# http://matplotlib.org/users/installing.html
+
+
+import sys
+from collections import defaultdict
+
+
+def readSeq(filename):
+    """reads in a FASTA sequence"""
+
+    stream = open(filename)
+    seq = []
+
+    for line in stream:
+        if line.startswith(">"):
+            continue
+        seq.append(line.rstrip())
+
+    return "".join(seq)
+
+
+def quality(hits, slope1, slope2, offset1, offset2):
+    """determines the quality of a list of hits"""
+
+    goodhits = []
+
+    for hit in hits:
+        upper = slope1 * hit[0] + offset1
+        lower = slope2 * hit[0] + offset2
+
+        if lower < hit[1] < upper:
+            goodhits.append(hit)
+
+    return goodhits
+
+
+def makeDotplot(filename, hits):
+    """generate a dotplot from a list of hits
+       filename may end in the following file extensions:
+         *.ps, *.png, *.jpg
+    """
+    import matplotlib.pyplot as plt
+    x, y = zip(*hits)
+
+    slope1 = 1.0e6 / (825000 - 48000)
+    slope2 = 1.0e6 / (914000 - 141000)
+    offset1 = 0 - slope1*48000
+    offset2 = 0 - slope2*141000
+
+    hits2 = quality(hits, slope1, slope2, offset1, offset2)
+    print("%.5f%% hits on diagonal" % (100 * len(hits2) / float(len(hits))))
+
+    x2 = [0, 1e6]
+    y2 = [offset1, slope1 * 1e6 + offset1]
+    y3 = [offset2, slope2 * 1e6 + offset2]
+
+    # create plot
+    plt.plot(x2, y2, 'b', x2, y3, 'g')
+    plt.scatter(x, y, s=1, c='r', edgecolors='none', marker=',')
+    plt.title("dotplot (%d hits, %.5f%% hits on diagonal)" %
+              (len(hits), 100 * len(hits2) / float(len(hits))))
+    plt.xlabel("sequence 1")
+    plt.ylabel("sequence 2")
+    plt.xlim(x2)
+    plt.ylim(x2)
+    plt.tight_layout()
+
+    # output plot
+    plt.savefig(filename)
+
+
+def main():
+
+    # parse command-line arguments
+    if len(sys.argv) < 4:
+        print("you must call program as:  ")
+        print("   ./ps1-2.py <FASTA 1> <FASTA 2> <PLOT FILE>")
+        print("   PLOT FILE may be *.ps, *.png, *.jpg")
+        sys.exit(1)
+
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+    plotfile = sys.argv[3]
+
+    # read sequences
+    print("reading sequences")
+    seq1 = readSeq(file1)
+    seq2 = readSeq(file2)
+
+    #
+    # You will need to modify the code below to
+
+    # length of hash key
+    kmerlen = 30
+
+    # hash table for finding hits
+    lookup = defaultdict(list)
+
+    # store sequence hashes in hash table
+    print("hashing seq1...")
+    for i in xrange(len(seq1) - kmerlen + 1):
+        key = seq1[i:i+kmerlen]
+        lookup[key].append(i)
+
+    # look up hashes in hash table
+    print("hashing seq2...")
+    hits = []
+    for i in xrange(len(seq2) - kmerlen + 1):
+        key = seq2[i:i+kmerlen]
+
+        # store hits to hits list
+        for hit in lookup.get(key, []):
+            hits.append((i, hit))
+
+    # hits should be a list of tuples
+    # [(index1_in_seq2, index1_in_seq1),
+    #  (index2_in_seq2, index2_in_seq1),
+    #  ...]
+
+    print("%d hits found" % len(hits))
+    print("making plot...")
+
+    makeDotplot(plotfile, hits)
+
+if __name__ == "__main__":
+    main()
---
+++ ps1-seqalign.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+import sys
+
+base_idx = {'A': 0, 'G': 1, 'C': 2, 'T': 3 }
+PTR_NONE, PTR_GAP1, PTR_GAP2, PTR_BASE = 0, 1, 2, 3
+
+
+def seqalignDP(seq1, seq2, subst_matrix, gap_penalty):
+    """
+    Return the score of the optimal Needleman-Wunsch alignment for seq1
+    and seq2.
+    Note: gap_penalty should be positive (it is subtracted)
+    """
+    F = [[0 for j in range(len(seq2)+1)] for i in range(len(seq1)+1)]
+    TB = [[PTR_NONE for j in range(len(seq2)+1)] for i in range(len(seq1)+1)]
+
+    # initialize dynamic programming table for Needleman-Wunsch alignment
+    # (Durbin p.20)
+    for i in range(1, len(seq1)+1):
+        F[i][0] = 0 - i*gap_penalty
+        TB[i][0] = PTR_GAP2  # indicates a gap in seq2
+    for j in range(1, len(seq2)+1):
+        F[0][j] = 0 - j*gap_penalty
+        TB[0][j] = PTR_GAP1  # indicates a gap in seq1
+
+
+    # Fill in the dynamic programming tables F and TB, starting at [1][1]
+    for j in range(1, len(F[0])):
+        for i in range(1, len(F)):
+            seq2_gap = F[i-1][j] - gap_penalty
+            seq1_gap = F[i][j-1] - gap_penalty
+            match_mism = F[i-1][j-1] + subst_matrix[base_idx[seq1[i-1]]][base_idx[seq2[j-1]]]
+            #F[i][j] = max(seq1_gap,seq2_gap,match_mism)
+            F[i][j] = min(seq1_gap, seq2_gap, match_mism)
+
+            if F[i][j]==match_mism:
+                TB[i][j] = PTR_BASE
+            elif F[i][j]==seq2_gap:
+                TB[i][j] = PTR_GAP2
+            elif F[i][j]==seq1_gap:
+                TB[i][j] = PTR_GAP1
+            else:
+                TB[i][j] = PTR_NONE
+                print("Traceback err")
+                assert False
+
+            #print(F[i][j])
+
+    #  (see for illustration Durbin p.21, Figure 2.5, but be careful what you
+    #   think of as rows and what you think of as columns)
+    #  Hence, the bases corresponding to F[i][j] are actually seq1[i-1] and
+    #  seq2[j-1].
+    #  Use the dictionary base_idx to convert from the character to an index to
+    #   look up entries of the substitution matrix.
+    #  To get started, you can complete and run the algorithm filling in only
+    #    F, and then figure out how to do TB.
+
+    return F[len(seq1)][len(seq2)], F, TB
+
+
+def traceback(seq1, seq2, TB):
+    s1 = ""
+    s2 = ""
+
+    i = len(seq1)
+    j = len(seq2)
+
+    while TB[i][j] != PTR_NONE:
+        if TB[i][j] == PTR_BASE:
+            s1 = seq1[i-1] + s1
+            s2 = seq2[j-1] + s2
+            i = i - 1
+            j = j - 1
+        elif TB[i][j] == PTR_GAP1:
+            s1 = '-' + s1
+            s2 = seq2[j-1] + s2
+            j = j - 1
+        elif TB[i][j] == PTR_GAP2:
+            s1 = seq1[i-1] + s1
+            s2 = '-' + s2
+            i = i - 1
+        else:
+            assert False
+
+    return s1, s2
+
+def readSeq(filename):
+    """Reads in a FASTA sequence. Assumes one sequence in the file"""
+    seq = []
+
+    with open(filename, "r") as f:
+        for line in f:
+            if line.startswith(">"):
+                continue
+            seq.append(line.rstrip().upper())
+
+    return "".join(seq)
+
+# Substitution matrix and gap_penalty
+# S = [
+#     # A   G   C   T
+#     [ 3, -1, -2, -2],  # A
+#     [-1,  3, -2, -2],  # G
+#     [-2, -2,  3, -1],  # C
+#     [-2, -2, -1,  3]   # T
+# ]
+S = [
+    #A  G  C  T
+    [0, 1, 2, 2],  # A
+    [1, 0, 2, 2],  # G
+    [2, 2, 0, 1],  # C
+    [2, 2, 1, 0]   # T
+]
+# gap_penalty = 4
+gap_penalty = -4
+
+def main():
+    # parse command line
+    if len(sys.argv) < 3:
+        print("Usage: {0} <FASTA 1> <FASTA 2>".format(sys.argv[0]))
+        sys.exit(1)
+
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+
+    seq1 = readSeq(file1)
+    seq2 = readSeq(file2)
+
+    score, F, TB = seqalignDP(seq1, seq2, S, gap_penalty)
+
+    print("Score: {0}".format(score))
+
+    s1, s2 = traceback(seq1, seq2, TB)
+    print(s1)
+    print(s2)
+
+if __name__ == "__main__":
+    main()
